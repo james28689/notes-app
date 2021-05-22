e3741481dc2d88a84a989c849b4df282b70c094c
Ben Williamson <benmacwill@gmail.com>
2021-05-22 18:05:43 +0100

    added tiny mce

--- src/CalendarView/calendar.js
+++ src/CalendarView/calendar.js
@@ -4,13 +4,13 @@ import arrow from "../Icons/arrow.svg";
 function Calendar( props ) {
     return (
         <div id="calendarView">
-            <div class="calendar">
-                <div class="calendarButtons">
+            <div className="calendar">
+                <div className="calendarButtons">
                     <h2>May 2021</h2>
                     <img src={arrow} alt="<"></img>
                     <img src={arrow} alt=">"></img>
                 </div>
-                <div class="days">
+                <div className="days">
                     <p>MON</p>
                     <p>TUE</p>
                     <p>WED</p>
@@ -18,41 +18,41 @@ function Calendar( props ) {
                     <p>FRI</p>
                     <p>SAT</p>
                     <p>SUN</p>
-                    <div class="day"><p>1</p></div>
-                    <div class="day"><p>2</p></div>
-                    <div class="day"><p>3</p></div>
-                    <div class="day"><p>4</p></div>
-                    <div class="day"><p>5</p></div>
-                    <div class="day"><p>6</p></div>
-                    <div class="day"><p>7</p></div>
-                    <div class="day"><p>8</p></div>
-                    <div class="day"><p>9</p></div>
-                    <div class="day"><p>10</p></div>
-                    <div class="day"><p>11</p></div>
-                    <div class="day"><p>12</p></div>
-                    <div class="day"><p>13</p></div>
-                    <div class="day"><p>14</p></div>
-                    <div class="day"><p>15</p></div>
-                    <div class="day"><p>16</p></div>
-                    <div class="day"><p>17</p></div>
-                    <div class="day"><p>18</p></div>
-                    <div class="day"><p>19</p></div>
-                    <div class="day"><p>20</p></div>
-                    <div class="day selected"><p>21</p></div>
-                    <div class="day"><p>22</p></div>
-                    <div class="day"><p>23</p></div>
-                    <div class="day"><p>24</p></div>
-                    <div class="day"><p>25</p></div>
-                    <div class="day"><p>26</p></div>
-                    <div class="day"><p>27</p></div>
-                    <div class="day"><p>28</p></div>
-                    <div class="day"><p>29</p></div>
-                    <div class="day"><p>30</p></div>
+                    <div className="day"><p>1</p></div>
+                    <div className="day"><p>2</p></div>
+                    <div className="day"><p>3</p></div>
+                    <div className="day"><p>4</p></div>
+                    <div className="day"><p>5</p></div>
+                    <div className="day"><p>6</p></div>
+                    <div className="day"><p>7</p></div>
+                    <div className="day"><p>8</p></div>
+                    <div className="day"><p>9</p></div>
+                    <div className="day"><p>10</p></div>
+                    <div className="day"><p>11</p></div>
+                    <div className="day"><p>12</p></div>
+                    <div className="day"><p>13</p></div>
+                    <div className="day"><p>14</p></div>
+                    <div className="day"><p>15</p></div>
+                    <div className="day"><p>16</p></div>
+                    <div className="day"><p>17</p></div>
+                    <div className="day"><p>18</p></div>
+                    <div className="day"><p>19</p></div>
+                    <div className="day"><p>20</p></div>
+                    <div className="day selected"><p>21</p></div>
+                    <div className="day"><p>22</p></div>
+                    <div className="day"><p>23</p></div>
+                    <div className="day"><p>24</p></div>
+                    <div className="day"><p>25</p></div>
+                    <div className="day"><p>26</p></div>
+                    <div className="day"><p>27</p></div>
+                    <div className="day"><p>28</p></div>
+                    <div className="day"><p>29</p></div>
+                    <div className="day"><p>30</p></div>
                 </div>
             </div>
-            <div class="timetable">
+            <div className="timetable">
             </div>
-            <div class="eventsDetails">
+            <div className="eventsDetails">
             </div>
         </div>
     )
--- src/Main/main.js
+++ src/Main/main.js
@@ -3,6 +3,7 @@ import "./Main.css";
 import Calendar from "../CalendarView/calendar";
 import React, {useEffect, useContext} from 'react';
 import {Context} from '../store';
+import { Editor } from '@tinymce/tinymce-react';
 
 function Main({ children }) {
 
@@ -14,11 +15,31 @@ function Main({ children }) {
             {state.selected === 0 &&
                 <div>
 
-                <h2>
+                {/* <h2>
                 Notes
                 </h2>
                 <p>This is the start of a design based off <a href="https://cdn.dribbble.com/users/65090/screenshots/14082104/media/8349b8ac58729f7ef4ae9b784081a824.png">this</a></p>
-                <p>Use this <a href="https://www.tiny.cloud/docs/quick-start/">this</a></p>
+                <p>Use <a href="https://www.tiny.cloud/docs/quick-start/">this</a></p> */}
+                
+                <Editor
+                apiKey="nluqabfk7ksn0arrulvq7smk1c0c64dnvy3iazhg6ajabrhx"
+         initialValue="<p>This is the initial content of the editor.</p>"
+         init={{
+        //    height: "100",
+           menubar: false,
+           plugins: [
+             'advlist autolink lists link image charmap print preview anchor',
+             'searchreplace visualblocks code fullscreen',
+             'insertdatetime media table paste code help wordcount autoresize'
+           ],
+           toolbar: 'undo redo | formatselect | ' +
+           'bold italic backcolor | alignleft aligncenter ' +
+           'alignright alignjustify | bullist numlist outdent indent | ' +
+           'removeformat | help',
+           content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+         }}
+       />
+                
                 </div>
             }
 
