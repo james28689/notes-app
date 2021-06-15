57f4ca40b09275ec979d44f241d8b7fd5536026f
Ben Williamson <benmacwill@gmail.com>
2021-06-15 15:08:54 +0100

    changed id to ID.

--- src/FolderTree/folderTree.js
+++ src/FolderTree/folderTree.js
@@ -27,12 +27,12 @@ function File(props) {
     const [state, dispatch] = useContext(Context);
 
     const openFile = () => {
-        dispatch({type: "SET_OPEN_FILE", payload: props.id});
+        dispatch({type: "SET_OPEN_FILE", payload: props.ID});
         dispatch({type: "SET_PAGE", payload: 0})
     }
 
     return (
-        <div className={"file " + ((props.id === state.openFile) ? "openFile" : "")} onClick= {openFile}>
+        <div className={"file " + ((props.ID === state.openFile) ? "openFile" : "")} onClick= {openFile}>
             {props.name}
         </div>
 
@@ -45,10 +45,10 @@ function RenderTree(props) {
         <div>
         {item.map((item, key) => {
             if(item.type === "folder") {
-                return <Folder id={item.id} key={key} name={ item.name } open={(item.children.length > 0 ? true : false)} children={<RenderTree item={item.children} ></RenderTree>}></Folder>
+                return <Folder ID={item.ID} key={key} name={ item.name } open={(item.children.length > 0 ? true : false)} children={<RenderTree item={item.children} ></RenderTree>}></Folder>
             }
             return (
-                <File key={key} name={ item.name } id={ item.id }></File>
+                <File key={key} name={ item.name } ID={ item.ID }></File>
             )
         }
         )}
@@ -60,15 +60,15 @@ function list_to_tree(list) {
     var map = {}, node, roots = [], i;
     
     for (i = 0; i < list.length; i += 1) {
-      map[list[i].id] = i; // initialize the map
+      map[list[i].ID] = i; // initialize the map
       list[i].children = []; // initialize the children
     }
     
     for (i = 0; i < list.length; i += 1) {
       node = list[i];
-      if (node.parentId !== null) {
-        // if you have dangling branches check that map[node.parentId] exists
-        list[map[node.parentId]].children.push(node);
+      if (node.parentID !== null) {
+        // if you have dangling branches check that map[node.parentID] exists
+        list[map[node.parentID]].children.push(node);
       } else {
         roots.push(node);
       }
