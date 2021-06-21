541dc1dab638076fc935462e7e6791f053fd31bf
Ben-Williamson <benmacwill@gmail.com>
2021-06-21 21:42:15 +0100

    test files.

--- src/store.js
+++ src/store.js
@@ -6,16 +6,16 @@ var d = new Date();
 
 const initialState = {
     selected: 0,
-    loggedIn: false,
+    loggedIn: true,
     selectedDay: d.getDay(),
     apiKey: null,
     theme: localStorage.getItem("theme") || "light",
 
     NotesData: [
-        // { id: 1, parentId: null, name: "School", type: "folder" },
-        // { id: 2, parentId: 1,    name: "Computing", type: "folder" },
-        // { id: 4, parentId: 1,    name: "Exams", type: "folder" },
-        // { id: 6, parentId: 4,    name: "Exam Timetable", type: "file", content: "test content"},
+        { id: 1, parentId: null, name: "School", type: "folder" },
+        { id: 2, parentId: 1,    name: "Computing", type: "folder" },
+        { id: 4, parentId: 1,    name: "Exams", type: "folder" },
+        { id: 6, parentId: 4,    name: "Exam Timetable", type: "file", content: "test content"},
     ],
     openFile: null
 };
